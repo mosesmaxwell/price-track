61686406b791b612cb28eb298e5bbb3d3df9ce1c
Moses Maxwell <moses.ephraiam-durai-raj@igate.com>
2016-04-20 05:52:26 +0000

    logs added

--- server/api/user.js
+++ server/api/user.js
@@ -5,15 +5,21 @@ var userSchema = require("../models/user.js");
 
 function createUser(req, res) {
   
+  console.log('Create user fn: ', req.body);
+  
   var userData = {
       username: req.body.username,
       password: req.body.password,
       email: req.body.email
   };  
+  console.log(userData);
   //Model name, schema, collection name
   var User = mongoose.model('User', userSchema, 'users');
+
   var user = new User();
+  console.log('user model created ', user);
     user.save(userData, function(error) {
+      console.log('user.save data called', error);
       if (error) {
         res.json({error: error});
         process.exit(1);
@@ -27,8 +33,9 @@ function getUser(req, res) {
 
 // public api
 module.exports = function (app) {
+  
     app.use(bodyparser.json());
-    
+  
     app.post('/server/api/user', createUser);
     app.get('/server/api/user', getUser);
     /*

de85b900591450a6a445b3b494b5a1a1c323a5ef
Moses Maxwell <moses.ephraiam-durai-raj@igate.com>
2016-04-20 06:52:29 +0000

    save signup final test1

--- server/models/user.js
+++ server/models/user.js
@@ -4,17 +4,14 @@ var mongoose = require("mongoose");
 module.exports = new mongoose.Schema({
     username: {
         type: String,
-        required: true,
         lowercase: true
     },
     password: {
         type: String,
-        required: true,
         lowercase: true
     },
     email: {
         type: String,
-        required: true,
         lowercase: true,
         match: /.+@.+\..+/
     }
